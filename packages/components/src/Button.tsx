841663360d276dc0170e5413436f528bcfc55c79
jonathan <jonathan.lapiquonne@gmail.com>
2022-04-09 18:28:34 -0400

    fix(Button): add more info

--- packages/components/src/Button.tsx
+++ packages/components/src/Button.tsx
@@ -3,7 +3,8 @@ import { useCount } from "mwaka/aria";
 
 const Button: Component = () => {
   const [count, { setCount }] = useCount();
-  return <button onClick={() => setCount(count() + 1)}>{count()}</button>;
+
+  return <button onClick={() => setCount(count() + 1)}>Click: {count()}</button>;
 };
 
 export default Button;

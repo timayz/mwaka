c4f4dbb62b546ccc79451601f65e94af900bf6b8
snapiz <jonathan.lapiquonne@gmail.com>
2022-05-04 00:17:38 +0200

    chore: remove pnpm due to incompatibility with vite (#5)

--- packages/components/src/Button.tsx
+++ packages/components/src/Button.tsx
@@ -4,7 +4,7 @@ import { useCount } from "mwaka/aria";
 const Button: Component = () => {
   const [count, { setCount }] = useCount();
 
-  return <button onClick={() => setCount(count() + 1)}>Click2: {count()}</button>;
+  return <button onClick={() => setCount(count() + 1)}>Click: {count()}</button>;
 };
 
 export default Button;

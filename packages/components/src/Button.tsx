0d296162ba895e42e3d4c4303c234d7a5645a570
snapiz <jonathan.lapiquonne@gmail.com>
2022-05-04 00:35:55 +0200

    fix(components): update button text

--- packages/components/src/Button.tsx
+++ packages/components/src/Button.tsx
@@ -4,7 +4,7 @@ import { useCount } from "mwaka/aria";
 const Button: Component = () => {
   const [count, { setCount }] = useCount();
 
-  return <button onClick={() => setCount(count() + 1)}>Click: {count()}</button>;
+  return <button onClick={() => setCount(count() + 1)}>Click me: {count()}</button>;
 };
 
 export default Button;

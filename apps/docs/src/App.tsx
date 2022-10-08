5a837377431d620fcb483d325ada18187fc9ff3c
snapiz <jonathan.lapiquonne@gmail.com>
2022-10-08 19:14:25 +0200

    chore: remove aria package (#4)

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -2,7 +2,11 @@ import type { Component } from "solid-js";
 import { Button } from "mwaka";
 
 const App: Component = () => {
-  return <Button />;
+  return (
+    <div>
+      <Button />
+    </div>
+  );
 };
 
 export default App;

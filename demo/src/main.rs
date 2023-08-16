2bd332bd3a275c3cfdb84e0afdc163d1cfd28910
snapiz <jonathan.lapiquonne@gmail.com>
2023-08-16 21:01:07 +0200

    tmp

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -2,10 +2,10 @@
 #[tokio::main]
 async fn main() {
     use axum::{routing::post, Router};
-    use leptos::*;
-    use leptos_axum::{generate_route_list, LeptosRoutes};
     use demo::app::*;
     use demo::fileserv::file_and_error_handler;
+    use leptos::*;
+    use leptos_axum::{generate_route_list, LeptosRoutes};
 
     simple_logger::init_with_level(log::Level::Debug).expect("couldn't initialize logging");
 
--- docs/src/main.rs
+++ docs/src/main.rs
@@ -2,10 +2,10 @@
 #[tokio::main]
 async fn main() {
     use axum::{routing::post, Router};
-    use leptos::*;
-    use leptos_axum::{generate_route_list, LeptosRoutes};
     use docs::app::*;
     use docs::fileserv::file_and_error_handler;
+    use leptos::*;
+    use leptos_axum::{generate_route_list, LeptosRoutes};
 
     simple_logger::init_with_level(log::Level::Debug).expect("couldn't initialize logging");
 
